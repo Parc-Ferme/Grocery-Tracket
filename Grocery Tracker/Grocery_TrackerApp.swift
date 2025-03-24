1e1871c979f1d3f7878a8471ae730d480a10101a
potter-18 <ankityadaav13@gmail.com>
2025-03-24 17:10:14 +0530

    Start new project for Grocery Tracker

---
+++ Grocery Tracker/Grocery_TrackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_TrackerApp.swift
+//  Grocery Tracker
+//
+//  Created by Ankit Yadav on 24/03/25.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_TrackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
